a78d76db629fd97a38ae33b654906d79d7e4fac3
MAHESWAR REDDY <iammaheshreddy.ios26@gmail.com>
2025-09-23 12:20:20 +0530

    Implemented the functionality for search to show only in Home page

--- SyriaBookingApp/Screens/Home/View/XIB.Cells/PromotionsCollectionViewCell.swift
+++ SyriaBookingApp/Screens/Home/View/XIB.Cells/PromotionsCollectionViewCell.swift
@@ -31,7 +31,7 @@ class PromotionsCollectionViewCell : UICollectionViewCell {
     }
     
     func configuration(with model: Hotel) {
-        destinationLabel.text = "\(model.localizedCity()), \(model.localizedCity())"
+        destinationLabel.text = "\(model.localizedName()), \(model.localizedCity())"
         activityInfoLabel.text = "\(model.reviewCount) Reviews, \(model.averageRating) Rating"
         if let firstImageURL = model.images.first, !firstImageURL.isEmpty {
             promotionHotelImageView.loadImage(from: firstImageURL)
